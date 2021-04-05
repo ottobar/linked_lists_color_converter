4b958753d0e0c8a46278c8baad2667631c9f4d33
Don Barlow <ottobar@gmail.com>
2021-04-05 19:19:54 +0000

    first commit

---
+++ lib/color_converter.ex
@@ -0,0 +1,18 @@
+defmodule ColorConverter do
+  @moduledoc """
+  Documentation for `ColorConverter`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ColorConverter.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/color_converter_test.exs
@@ -0,0 +1,8 @@
+defmodule ColorConverterTest do
+  use ExUnit.Case
+  doctest ColorConverter
+
+  test "greets the world" do
+    assert ColorConverter.hello() == :world
+  end
+end
